1766bfe5ff14526a6b9033d7de999a8586f99f1e
Matheus Santos <vorj.dux@gmail.com>
2018-02-18 05:00:24 +0000

    Fixing problems and dependences to compile on PPA Server

--- mongodb/include/zapata/mongodb/config.h
+++ mongodb/include/zapata/mongodb/config.h
@@ -96,7 +96,7 @@
 #define PACKAGE_NAME "zapata-mongodb"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "zapata-mongodb 2.0.0-1artful7"
+#define PACKAGE_STRING "zapata-mongodb 2.0.0-1artful8"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "zapata-mongodb"
@@ -105,10 +105,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.0-1artful7"
+#define PACKAGE_VERSION "2.0.0-1artful8"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.0.0-1artful7"
+#define VERSION "2.0.0-1artful8"
